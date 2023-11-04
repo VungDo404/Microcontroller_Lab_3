5914026714e437723230d8e40fcb7075b2ff251b
Thanh Vung <thanhvungcbt@gmail.com>
2023-11-04 22:45:14 +0700

    commit

--- Source/Core/Inc/main.h
+++ Source/Core/Inc/main.h
@@ -58,6 +58,46 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_HORI_Pin GPIO_PIN_1
+#define LED_RED_HORI_GPIO_Port GPIOA
+#define LED_YELLOW_HORI_Pin GPIO_PIN_2
+#define LED_YELLOW_HORI_GPIO_Port GPIOA
+#define LED_GREEN_HORI_Pin GPIO_PIN_3
+#define LED_GREEN_HORI_GPIO_Port GPIOA
+#define LED_GREEN_VERTI_Pin GPIO_PIN_4
+#define LED_GREEN_VERTI_GPIO_Port GPIOA
+#define LED_YELLOW_VERTI_Pin GPIO_PIN_5
+#define LED_YELLOW_VERTI_GPIO_Port GPIOA
+#define LED_GREEN_VERTIA6_Pin GPIO_PIN_6
+#define LED_GREEN_VERTIA6_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_7
+#define EN0_GPIO_Port GPIOA
+#define SEG0_Pin GPIO_PIN_0
+#define SEG0_GPIO_Port GPIOB
+#define SEG1_Pin GPIO_PIN_1
+#define SEG1_GPIO_Port GPIOB
+#define SEG2_Pin GPIO_PIN_2
+#define SEG2_GPIO_Port GPIOB
+#define EN1_Pin GPIO_PIN_8
+#define EN1_GPIO_Port GPIOA
+#define EN2_Pin GPIO_PIN_9
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_10
+#define EN3_GPIO_Port GPIOA
+#define BUTTON1_Pin GPIO_PIN_13
+#define BUTTON1_GPIO_Port GPIOA
+#define BUTTON2_Pin GPIO_PIN_14
+#define BUTTON2_GPIO_Port GPIOA
+#define BUTTON3_Pin GPIO_PIN_15
+#define BUTTON3_GPIO_Port GPIOA
+#define SEG3_Pin GPIO_PIN_3
+#define SEG3_GPIO_Port GPIOB
+#define SEG4_Pin GPIO_PIN_4
+#define SEG4_GPIO_Port GPIOB
+#define SEG5_Pin GPIO_PIN_5
+#define SEG5_GPIO_Port GPIOB
+#define SEG6_Pin GPIO_PIN_6
+#define SEG6_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
